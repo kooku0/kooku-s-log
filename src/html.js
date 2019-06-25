d7ad76a7d934680844826c4a883d36dbb68d97da
Kooku <koomg9599@gmail.com>
2019-06-26 00:43:04 +0900

    setting search-console

--- src/html.js
+++ src/html.js
@@ -12,6 +12,10 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
           />
+          <meta
+            name="google-site-verification"
+            content="EFWY1tFw9rE53Le49JwUBZA-f199rj7MRsUQMRBT-jo"
+          />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
