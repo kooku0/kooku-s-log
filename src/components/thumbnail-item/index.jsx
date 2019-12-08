0ca46a148ff4ad470fbc1c03f9eded767c0cd344
Kooku <koomg9599@gmail.com>
2019-12-08 14:39:50 +0900

    feat: thumbnail card 형식으로 변경

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
+import { divideDateAndPath } from '../../utils/frontmatter'
 
 import './index.scss'
 
-export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-    </div>
-  </Link>
-)
+export const ThumbnailItem = ({ node }) => {
+  const [date, path] = useMemo(() => divideDateAndPath(node.field.slug), [
+    nodef,
+  ])
+  return (
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={path}>
+      <article key={node.fields.slug}>
+        <h3>
+          {node.frontmatter.title}
+          <time className="thumbnail-date" dateTime={date}>
+            {date}
+          </time>
+        </h3>
+        <article dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </article>
+    </Link>
+  )
+}
