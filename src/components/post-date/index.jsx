d520e584f006b270c652385e53bc0788c04efb12
Kooku <koomg9599@gmail.com>
2020-01-27 11:59:12 +0900

    feat: post-date 추가

---
+++ src/components/post-date/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import './index.scss'
+
+export const PostDate = ({ date }) => <p className="post-date">{date}</p>
--- src/components/post-title/index.jsx
+++ src/components/post-title/index.jsx
@@ -1,8 +1,3 @@
 import React from 'react'
 
-export const PostTitle = ({ title, date }) => (
-  <h1 className="post-title">
-    {title}
-    <time dateTime={date}>{date}</time>
-  </h1>
-)
+export const PostTitle = ({ title }) => <h1 className="post-title">{title}</h1>
